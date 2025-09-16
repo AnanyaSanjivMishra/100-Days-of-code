c3492cd5ccad679411be77ba5dbb1a49d1e608f0
Ananya Sanjiv Mishra <anany.s.mishra@gmail.com>
2025-09-16 19:55:17 +0530

    Add files via upload

---
+++ P53 C.cpp
@@ -0,0 +1,30 @@
+//Q53: Write a program to print the following pattern:
+/*
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+*/
+
+#include <stdio.h>
+int main() {
+    int i, j, n = 7; 
+    for (i = 1; i <= n; i += 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = n - 2; i >= 1; i -= 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
