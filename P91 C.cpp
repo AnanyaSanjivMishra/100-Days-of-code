3df045afc71545dfd5bc4393028ff9aa84d59eb1
Ananya Sanjiv Mishra <anany.s.mishra@gmail.com>
2025-10-05 17:37:40 +0530

    Add files via upload

---
+++ P91 C.cpp
@@ -0,0 +1,24 @@
+//Q91: Remove all vowels from a string.
+#include <stdio.h>
+#include <string.h>
+void removeVowels(char *str) {
+    int i, j = 0;
+    char temp[strlen(str) + 1];
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' &&
+            str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
+            temp[j++] = str[i];
+        }
+    }
+    temp[j] = '\0';
+    strcpy(str, temp);
+}
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = 0;  // Remove newline character
+    removeVowels(str);
+    printf("String after removing vowels: %s\n", str);
+    return 0;
+}
